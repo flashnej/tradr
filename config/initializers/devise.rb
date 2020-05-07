7d88e2f24b3ba27868b5fa746d077bf5274985ab
flashnej <flashnej@gmail.com>
2020-05-07 19:46:26 -0400

    tried using the docs on changing the redirect, didnt quite get it

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,10 +266,11 @@ Devise.setup do |config|  config.secret_key = Rails.application.secret_key_base
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  # config.warden do |manager|
-  #   manager.intercept_401 = false
-  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
-  # end
+  config.warden do |manager|
+    manager.intercept_401 = false
+    manager.default_strategies(scope: :user).unshift :some_external_strategy
+    manager.failure_app = MyFailureApp
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
